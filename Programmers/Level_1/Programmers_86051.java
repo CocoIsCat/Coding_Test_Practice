30a34d735b849cc5f4b8ccb925de45883c556859
cocofarm <demy176@naver.com>
2022-07-27 16:29:33 +0900

    없는 숫자 더하기

---
+++ Programmers/Level_1/Programmers_86051.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+
+        for(int i : numbers) {
+            answer -= i;
+        }
+        return answer;
+    }
+}
