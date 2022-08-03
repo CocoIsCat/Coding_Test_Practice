306b5acac4e7f4acdce81de9cad96c4ff7ff0d27
cocofarm <demy176@naver.com>
2022-08-03 21:00:56 +0900

    15829번 - Hashing

---
+++ BEAKJOON/Java/Bronze/BOJ_15829.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        int r = 31;
+        int m = 1234567891;
+        long sum = 0;
+        long mod = 1;
+        char[] ch = br.readLine().toCharArray();
+
+        for(int i = 0; i < length; i++) {
+
+            sum += (((int)((ch[i] - 'a') + 1)) * mod);
+            mod = (r * mod) % m;
+        }
+        System.out.println(sum % m);
+    }
+}
