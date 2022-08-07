fae93328d20da741d6e7e2dda1e8a58d11f95508
cocofarm <demy176@naver.com>
2022-08-07 22:31:50 +0900

    2420번 - 사파리월드

---
+++ BEAKJOON/Java/Bronze/BOJ_2420.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        long m = sc.nextInt();
+        System.out.println(Math.abs(n - m));
+    }
+}
