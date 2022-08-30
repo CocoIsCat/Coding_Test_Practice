d4b0aa4d917faef1b3ce87e662e90557f235b09b
cocofarm <demy176@naver.com>
2022-08-30 15:43:23 +0900

    1406번 - 에디터

--- BEAKJOON/Java/Silver/BOJ_1406.java
+++ BEAKJOON/Java/Silver/BOJ_1406.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class BOJ_1406 {
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> right = new Stack<>();
+        Stack<Character> left = new Stack<>();
+        String ini = bf.readLine();
+        int M = Integer.parseInt(bf.readLine());
+        for(char ch : ini.toCharArray()) {
+            left.push(ch);
+        }
+
+        while(M-- > 0) {
+            String cmd = bf.readLine();
+            String[] op = cmd.split(" ");
+            char tmp;
+            if(op[0].charAt(0) == 'L') {
+                if(!left.isEmpty()) {
+                    tmp = left.pop();
+                    right.push(tmp);
+                }
+            }
+            else if(op[0].charAt(0) == 'D') {
+                if(!right.isEmpty()) {
+                    tmp = right.pop();
+                    left.push(tmp);
+                }
+            }
+            else if(op[0].charAt(0) == 'B') {
+                if(!left.isEmpty()) {
+                    left.pop();
+                }
+            }
+            else if(op[0].charAt(0) == 'P') {
+                left.push(op[1].charAt(0));
+            }
+        }
+        for(char ch : left) {
+            sb.append(ch);
+        }
+        while(!right.isEmpty()) {
+            sb.append(right.pop());
+        }
+        System.out.println(sb);
+    }
 }
