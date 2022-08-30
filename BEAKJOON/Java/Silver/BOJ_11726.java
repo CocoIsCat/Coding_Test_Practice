ca89690bdf9cb879ee842202360418b3b1be51e6
cocofarm <demy176@naver.com>
2022-08-30 15:33:04 +0900

    11726번- 2Xn 타일링

--- BEAKJOON/Java/Silver/BOJ_11726.java
+++ BEAKJOON/Java/Silver/BOJ_11726.java
@@ -8,7 +8,8 @@ class Main{
         memo[1] = 1;
         for(int i = 2; i <= n; i++) {
             memo[i] = memo[i - 2] + memo[i - 1];
+            memo[i] %= 10007;
         }
-        System.out.println(memo[n] % 10007);
+        System.out.println(memo[n]);
     }
 }
