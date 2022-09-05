5f7614b278162d190f2eecca7583955047197ac6
cocofarm <demy176@naver.com>
2022-09-05 22:22:15 +0900

    2743번 - 단어 길이 재기

---
+++ BEAKJOON/Java/Bronze/BOJ_2743.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        String str = new Scanner(System.in).next();
+        System.out.println(str.length());
+    }
+}
